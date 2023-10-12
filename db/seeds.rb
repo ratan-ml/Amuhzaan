198664eb1b256610e30c9632aa869bda1037b66b
Raymond <raymondtan676@gmail.com>
2023-10-12 11:28:47 -0700

    testing render users

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,21 +9,22 @@
 # ApplicationRecord.transaction do 
     puts "Destroying tables..."
     # Unnecessary if using `rails db:seed:replant`
-    User.destroy_all
     Product.destroy_all
+    User.destroy_all
+
 
     puts "Resetting primary keys..."
     # For easy testing, so that after seeding, the first `User` has `id` of 1
     ApplicationRecord.connection.reset_pk_sequence!('users')
     ApplicationRecord.connection.reset_pk_sequence!('products')
-
-    puts "Creating users..."
+    
+    puts "Creating demo user..."
     User.create!(
         name: 'demo',
         email: 'demo@user.io', 
         password: 'password'
     )
-
+    puts "creating more users"
     # More users
     10.times do 
         User.create!({
