f55a1ee660a37d1eaad4b5af3d687d690536411b
Raymond <raymondtan676@gmail.com>
2023-10-16 19:05:06 -0700

    Complete basic home page

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,9 +1,27 @@
+import './Footer.css'
+
 const Footer = () => {
 
     return (
-        <div className="footer-containter">
-            <p>Languag</p>
-        </div>
+        <footer className="footer">
+            <div className="footer-containter">
+                <p className="credit-resource">
+                    Banner by <a href="https://www.freepik.com/free-vector/flat-black-friday-twitch-cover_18494357.htm#query=amazon%20banner&position=4&from_view=keyword&track=ais">Freepik</a>
+                </p>
+            </div>
+            <div className="built-from">
+                <span>Built from:</span>
+                <span>HTML</span>
+                <span>CSS</span>
+                <span>JavaScript</span>
+                <span>React</span>
+                <span>Rails</span>
+            </div>
+
+            <div className="trademark">
+                    <p>@October 2023, Amuhzaan is a Amzaon Clone created by Raymond Tan</p>
+                </div>
+        </footer>
     )
 }
 
--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import banner1 from '../../assets/home-banner-1.1.jpg';
 import './HomePage.css';
+import Footer from "../Footer";
 
 const HomePage = () => {
 
@@ -9,6 +10,7 @@ const HomePage = () => {
             <div className="banner-container">
                 <img src={banner1} alt=""/>
             </div>
+            <Footer/>
         </>
     )
 }
